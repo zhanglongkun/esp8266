9c388f73b388d9bc75b0cd4ed4aac0619969e3b0
root <root@ubuntu.(none)>
2017-05-03 11:49:52 +0800

    add TCP server

--- et_app/user/user_main.c
+++ et_app/user/user_main.c
@@ -4,7 +4,7 @@
   * @Author:       zlk
   * @Version:      V1.0
   * @Date:         2017-5-1 下午 4:29:41
-  * @Description:  This file provides all the user_main.c functions. 
+  * @Description:  This file provides all the user_main.c functions.
   ******************************************************************************
   */
 
@@ -16,8 +16,9 @@
 #define AP_NAME        "JQM-206"
 #define AP_PASSWORD    "fjjqm123"
 
-#define SERVER_IP      "192.168.1.102"
-#define SERVER_PORT    8888
+#define TCP_SERVER_IP      "192.168.1.102"
+#define TCP_SERVER_PORT    8888
+#define LISTEN_MAX_CONN    10
 
 /**
   ******************************************************************************
@@ -26,7 +27,7 @@
   * Date:         2017-05-01
   * Others:       add by zlk
   ******************************************************************************
-  */ 
+  */
 bool Set_WIFI_Option()
 {
     bool ret = TRUE;
@@ -43,14 +44,14 @@ bool Set_WIFI_Option()
     sprintf(configWIFI->ssid, AP_NAME);
     sprintf(configWIFI->password, AP_PASSWORD);
 
-	ETS_UART_INTR_DISABLE();
-	ret = wifi_station_set_config(configWIFI);                  //设置WIFI连接
+    ETS_UART_INTR_DISABLE();
+    ret = wifi_station_set_config(configWIFI);                  //设置WIFI连接
     if (FALSE == ret) {
         os_printf("wifi_station_set_config() error\n");
         goto EXIT;
     }
-	ETS_UART_INTR_ENABLE();
-    
+    ETS_UART_INTR_ENABLE();
+
     ret = wifi_station_connect();
     if (FALSE == ret) {
         os_printf("wifi_station_connect() error\n");
@@ -61,11 +62,65 @@ EXIT:
     if (NULL != configWIFI) {
         free(configWIFI);
     }
-    
+
     return ret;
 }
 
-bool Build_Client_Socket()
+bool Build_TCP_Servers()
+{
+    int ret;
+    int listenfd, clientfd;
+    int clientAddrLen, bufLen;
+    char recvbuf[20] = {0};
+    struct sockaddr_in servAddr, remoteAddr;
+
+    memset(&servAddr, 0, sizeof(struct sockaddr_in));
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == listenfd) {
+        os_printf("creat socket failed, line = %d\n", __LINE__);
+        return FALSE;
+    }
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+    servAddr.sin_port = htons(TCP_SERVER_PORT);
+    servAddr.sin_len = sizeof(struct sockaddr_in);
+
+    ret = bind(listenfd, (struct sockaddr *)&servAddr, sizeof(struct sockaddr_in));
+    if (0 != ret) {
+        os_printf("bind failed, ret = %d, line = %d\n", ret, __LINE__);
+        return FALSE;
+    }
+
+    ret = listen(listenfd, LISTEN_MAX_CONN);
+    if (0 != ret) {
+        os_printf("listen failed, ret = %d, line = %d\n", ret, __LINE__);
+        return FALSE;
+    }
+
+    clientAddrLen = sizeof(struct sockaddr_in);
+
+    for (; ;) {
+        memset(&remoteAddr, 0, sizeof(struct sockaddr_in));
+
+        os_printf("ESP8266 TCP server task > wait client\n");
+        clientfd = accept(listenfd, (struct sockaddr *)&remoteAddr, &clientAddrLen);
+        if (clientfd < 0) {
+            continue;
+        }
+
+        os_printf("remote addr:%s, port = %d\n", inet_ntoa(remoteAddr.sin_addr), ntohs(remoteAddr.sin_port));
+
+        bufLen = read(clientfd, recvbuf, sizeof(recvbuf));
+        os_printf("buf len = %d\n", bufLen);
+        if (bufLen > 0) {
+            os_printf("recv buffer %s\n", recvbuf);
+        }
+    }
+}
+
+bool Build_TCP_Client()
 {
     int socketfd;
     char buf[] = "aaaaaaa";
@@ -80,8 +135,8 @@ bool Build_Client_Socket()
     }
 
     clientAddr.sin_family = AF_INET;
-    clientAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    clientAddr.sin_port = htons(SERVER_PORT);
+    clientAddr.sin_addr.s_addr = inet_addr(TCP_SERVER_IP);
+    clientAddr.sin_port = htons(TCP_SERVER_PORT);
 
     while (1) {
         if (0 != connect(socketfd, (struct sockaddr *)&clientAddr, sizeof(struct sockaddr_in))) {
@@ -92,7 +147,7 @@ bool Build_Client_Socket()
         os_printf("connect succeed\n");
         break;
     }
-    
+
 #if 0
     if (0 != connect(socketfd, (struct sockaddr *)&clientAddr, sizeof(struct sockaddr_in))) {
         os_printf("connect error\n");
@@ -104,10 +159,58 @@ bool Build_Client_Socket()
         os_printf("write error\n");
         return -1;
     }
-    
+
     os_printf(".................\n");
     return 0;
-        
+
+}
+
+void wifi_handle_event_cb(System_Event_t *evt)
+{
+    printf("event %x\n", evt->event_id);
+    
+    switch (evt->event_id) {
+        case EVENT_STAMODE_CONNECTED:
+            printf("connect to ssid	%s,	channel	%d\n",
+                   evt->event_info.connected.ssid,
+                   evt->event_info.connected.channel);
+            break;
+            
+        case EVENT_STAMODE_DISCONNECTED:
+            printf("disconnect	from ssid %s, reason %d\n",
+                   evt->event_info.disconnected.ssid,
+                   evt->event_info.disconnected.reason);
+            break;
+            
+        case EVENT_STAMODE_AUTHMODE_CHANGE:
+            printf("mode: %d -> %d\n",
+                   evt->event_info.auth_change.old_mode,
+                   evt->event_info.auth_change.new_mode);
+            break;
+            
+        case EVENT_STAMODE_GOT_IP:
+            printf("ip:" IPSTR ",mask:" IPSTR ",gw:" IPSTR,
+                   IP2STR(&evt->event_info.got_ip.ip),
+                   IP2STR(&evt->event_info.got_ip.mask),
+                   IP2STR(&evt->event_info.got_ip.gw));
+            printf("\n");
+            break;
+            
+        case EVENT_SOFTAPMODE_STACONNECTED:
+            printf("station: " MACSTR "join, AID = %d\n",
+                   MAC2STR(evt->event_info.sta_connected.mac),
+                   evt->event_info.sta_connected.aid);
+            break;
+            
+        case EVENT_SOFTAPMODE_STADISCONNECTED:
+            printf("station: " MACSTR "leave, AID = %d\n",
+                   MAC2STR(evt->event_info.sta_disconnected.mac),
+                   evt->event_info.sta_disconnected.aid);
+            break;
+            
+        default:
+            break;
+    }
 }
 
 /**
@@ -117,11 +220,11 @@ bool Build_Client_Socket()
   * Date:         2017-05-01
   * Others:       add by zlk
   ******************************************************************************
-  */ 
+  */
 void user_init(void)
-{	
+{
     bool ret = TRUE;
-    
+
     os_printf("software version: %s\n", system_get_sdk_version()); //打印软件版本号
     os_printf("ESP8266 chip ID: 0x%x\n", system_get_chip_id()); //打印芯片ID
 
@@ -134,6 +237,7 @@ void user_init(void)
     os_printf("11111\n");
     vTaskDelay(500);
     os_printf("222222\n");
+    wifi_set_event_handler_cb(wifi_handle_event_cb);
 
 #if 0
     ret = Build_Client_Socket();
@@ -141,5 +245,5 @@ void user_init(void)
         os_printf("Build_Client_Socket() error\n");
     }
 #endif
-    
+
 }
